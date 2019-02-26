0b9206480bfb400599a67fe0715f18b37fca72f1
Helder Vilela <dev@heldervilela.com>
2019-02-26 09:56:15 -0100

    Webpack: Inline Svg

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
-const path              = require( 'path' );
-const withSass = require('@zeit/next-sass');
+const path     = require( 'path' );
+const withSass = require( '@zeit/next-sass' );
 
-module.exports = withSass({
+module.exports = withSass( {
 	sassLoaderOptions: {
-		data: '@import "'+path.resolve( __dirname, 'static/styles/_common.scss' )+'";\n',
+		data: '@import "' + path.resolve( __dirname, 'static/styles/_common.scss' ) + '";\n',
+	},
+	webpack(config, options) {
+		config.module.rules.push(
+			{
+				test: /\.svg$/,
+				use: [
+					{ loader: "babel-loader" },
+					{
+						loader: "react-svg-loader",
+						options: {
+							jsx: true // true outputs JSX tags
+						}
+					}
+				]
+			}
+		);
+		return config
 	}
-});
+} );
+
+
+//
+// conf.module.rules.push(
+
+// );
